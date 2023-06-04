941f035f61da0cfb125f27c9c18e5691a96b63ce
karpekin <karpekin@mail.ru>
2023-06-04 12:50:11 +0300

    Get length array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Контрольная работа: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, либо равна
+// 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать
+// на старте выполнения алгоритма. При решении не рекомендуется пользоваться
+// коллекциями, лучше обойтись исключительно массивами.
+
+string[] array = new string[] {"Hello", "2-", "world", ":-)"};
+GetLengthArray(array);
+
+int GetLengthArray(string[] array)
+{
+int l = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        l = l + 1;
+    }
+}
+return l;
+}
